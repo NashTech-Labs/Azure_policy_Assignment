3d8b1eb0e1567504f204e35fc53392a29e4754aa
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-09 16:31:53 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -13,29 +13,16 @@ terraform {
   }
 
 }
-
-
-
-
 provider "azurerm" {
-
-  features {
-
-
-
-
-  }
-
+  features {}  
 }
-
-resource "azurerm_policy_definition" "addTagToRG" {
-  #count = length(var.mandatory_tag_keys)
-
-  name         = "addTagToRG_${var.mandatory_tag_key}"
+resource "azurerm_policy_definition" "TagForRG" {
+ 
+  name         = var.mandatory_tag_key
   policy_type  = "Custom"
   mode         = "All"
-  display_name = "Add tag ${var.mandatory_tag_key} to resource group"
-  description  = "Adds the mandatory tag key ${var.mandatory_tag_key} when any resource group missing this tag is created or updated. \nExisting resource groups can be remediated by triggering a remediation task.\nIf the tag exists with a different value it will not be changed."
+  display_name = var.mandatory_tag_key
+  #description  = var.mandatory_tag_key  
 
   metadata = <<METADATA
     {
@@ -44,36 +31,38 @@ resource "azurerm_policy_definition" "addTagToRG" {
     }
 METADATA
 
-  policy_rule = <<POLICY_RULE
+      policy_rule = <<POLICY_RULE
     {
-        "if": {
-          "allOf": [
-            {
-              "field": "type",
-              "equals": "Microsoft.Resources/subscriptions/resourceGroups"
-            },
+              "if": {
+        "allOf": [
+          {
+            "equals": "Microsoft.Resources/subscriptions/resourceGroups",
+            "field": "type"
+          },
+          {
+            "exists": "false",
+            "field": "[concat('tags[', parameters('tagName'), ']')]"
+          }
+        ]
+      },
+      "then": {
+        "details": {
+          "operations": [
             {
               "field": "[concat('tags[', parameters('tagName'), ']')]",
-              "exists": "false"
+              "operation": "add",
+              "value": "[parameters('tagValue')]"
             }
+          ],
+          "roleDefinitionIds": [
+            "/providers/microsoft.authorization/roleDefinitions/0f2e685f-410b-4d1a-a18c-83ae2efaad95"
           ]
         },
-        "then": {
-          "effect": "modify",
-          "details": {
-            "roleDefinitionIds": [
-              "/providers/microsoft.authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c"
-            ],
-            "operations": [
-              {
-                "operation": "add",
-                "field": "[concat('tags[', parameters('tagName'), ']')]",
-                "value": "[parameters('tagValue')]"
-              }
-            ]
-          }
-        }
-  }
+        "effect": "modify"
+      }
+    }
+  
+
 POLICY_RULE
 
 
@@ -90,16 +79,19 @@ POLICY_RULE
         "tagValue": {
           "type": "String",
           "metadata": {
-            "displayName": "Tag Value '${var.mandatory_tag_value}'",
-            "description": "Value of the tag, such as '${var.mandatory_tag_value}'"
+            "displayName": "Tag Value '${var.mandatory_tag}'",
+            "description": "Value of the tag, such as '${var.mandatory_tag}'"
           },
-          "defaultValue": "'${var.mandatory_tag_value}'"
+          "defaultValue": "'${var.mandatory_tag}'"
         }
   }
 PARAMETERS
 
 }
 resource "azurerm_subscription_policy_assignment" "tag_governance" {
-  name                 = "tag_governance"
+  name                 = var.tag_governance
   description          = "Assignment of the Tag Governance policy to subscription."
-  policy_definition_id = azurerm_policy_definition.addTagToRG.id
+  policy_definition_id = azurerm_policy_definition.TagForRG.id
+  subscription_id      = data.azurerm_subscription.current.id
+  identity { type = var.identity }
+  location = var.location
